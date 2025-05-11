f29896dce994aff0be8be57866fd0a8f0963ea4c
Mark Wutka <mark@wutka.com>
2025-05-10 22:51:13 -0400

    Fix logic for endless loop detection to make sure node in loop has no path to end

--- analyzers/loop.py
+++ analyzers/loop.py
@@ -3,17 +3,26 @@ from SoluminaImport.class_model import *
 from path_utils import enumerate_paths
 import fault
 
-class PathAnalyzer(Analyzer):
+class LoopAnalyzer(Analyzer):
     def __init__(self):
         super().__init__()
 
     def analyze(self, model, fault_list):
         paths = enumerate_paths(model)
 
+        reaches_end = set()
+        for curr_path in paths:
+            if isinstance(curr_path[-1], EndEvent):
+                for item in curr_path:
+                    if hasattr(item, 'bplElementId'):
+                        reaches_end.add(item.bplElementId)
+
         reported = set()
         for curr_path in paths:
             if not isinstance(curr_path[-1], EndEvent):
                 id = curr_path[-1].bplElementId
+                if id in reaches_end: continue
+
                 if id not in reported:
                     reported.add(id)
                     fault_list.append(
